08ed78f27824a8a4cd83499649235729551a7719
YunYouJun <me@yunyoujun.cn>
2021-05-11 21:16:20 +0800

    :chart_with_upwards_trend: add vue-gtag

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import VueGtag from "vue-gtag";
 
 import { ElRadioGroup, ElRadio, ElRadioButton, ElButton } from "element-plus";
 // import "element-plus/lib/theme-chalk/index.css";
 import "element-theme-ink";
 
 const app = createApp(App);
+app.use(VueGtag, {
+  config: {
+    id: "G-FMH4WDW53W",
+  },
+});
 app.use(ElButton);
 app.use(ElRadioGroup);
 app.use(ElRadio);
