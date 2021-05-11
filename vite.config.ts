6d86e961b649f9d6870d01529ceb6af886241ac9
YunYouJun <me@yunyoujun.cn>
2021-05-11 16:15:06 +0800

    :sparkles: add vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import styleImport from "vite-plugin-style-import";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,6 +23,32 @@ export default defineConfig({
           }
         }
       ]
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Electric Fan",
+        short_name: "EF",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable"
+          }
+        ]
+      }
     })
   ]
 });
